8b766bfa2a0226bfa8b6de4f25b2ecfd423cb6ba
Lucas Martins <lucas03188@hotmail.com>
2019-08-30 20:27:20 -0300

    tela de logim=n modificada

--- login.php
+++ login.php
@@ -16,49 +16,61 @@
   <meta name="description" content="" />
   <meta name="keywords" content="" />
   <link href="stylesheets/locastyle.css" rel="stylesheet" type="text/css">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="css/estilo.css">
+  <style>
+  
+  </style>
 </head>
 <body class="documentacao documentacao_exemplos documentacao_exemplos_login-screen documentacao_exemplos_login-screen_index">
+  
+<div id="card-form" class="conteiner fluid">
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-4">
+      <div  class="card border border-1 ">
+        <img src="images/logo_login.png" class="card-img-top">
+          <div class="card-body">
+            <div class="form-group">
+              <div class="row">
+                <div class="col-2"></div>
+                <div class="col-8">
+                  <form method="post" action="lib/autentica.php">
+                  <label class="ls-label">
+                  <b class="text-success"> Usuário </b>
+                  <input class="ls-login-bg-user ls-field-lg" name="email" type="text" placeholder="Seu email professor" required autofocus>
+                  </label>
+                  <label class="ls-label">
+                    <b class="text-success">Senha</b> 
+                    <div class="ls-prefix-group ls-field-lg">
+                      <input id="password_field" class="ls-login-bg-password" name="pass" type="password" placeholder="Sua Senha" required>
+                      <a class="ls-label-text-prefix ls-toggle-pass ls-ico-eye" data-toggle-class="ls-ico-eye, ls-ico-eye-blocked" data-target="#password_field" href="#"></a>
+                    </div>
+                  </label>
 
-<div class="ls-login-parent">
-  <div class="ls-login-inner">
-    <div class="ls-login-container">
- 
-      <div class="ls-login-box">
-  <h1 class="ls-login-logo">
-   <a href="login.php"> <img title="Logo login" src="images/login.png" height="145" width="240" /></a>
-  </h1>
-  <h3><center>Sistema de Uploads de Arquivos</center></h3>
-  <br>
-  <form role="form" class="ls-form ls-login-form" method="post" action="lib/autentica.php">
-    <fieldset>
+                  <p><a class="ls-login-forgot" href="#">Esqueci minha senha</a></p>
+                  <br>  
+                  <input type="submit" value="Entrar" class="btn btn-success ls-btn-block btn-lg">
 
-      <label class="ls-label">
-        <b class="ls-label-text ls-hidden-accessible">Usuário</b>
-        <input class="ls-login-bg-user ls-field-lg" name="email" type="text" placeholder="Seu email" required autofocus>
-      </label>
-
-      <label class="ls-label">
-        <b class="ls-label-text ls-hidden-accessible">Senha</b>
-        <div class="ls-prefix-group ls-field-lg">
-          <input id="password_field" class="ls-login-bg-password" name="pass" type="password" placeholder="Senha" required>
-          <a class="ls-label-text-prefix ls-toggle-pass ls-ico-eye" data-toggle-class="ls-ico-eye, ls-ico-eye-blocked" data-target="#password_field" href="#"></a>
+                  </form>
+                </div>
+                <div class="col-2"></div>
+                </div>
+            </div>
+          </div>
         </div>
-      </label>
-
-      <p><a class="ls-login-forgot" href="#">Esqueci minha senha</a></p>
-
-      <input type="submit" value="Entrar" class="ls-btn-primary ls-btn-block ls-btn-lg">
-     
-    </fieldset>
-  </form>
-</div>
-
     </div>
+    <div class="col-4"></div>
   </div>
 </div>
 
+
  <script type="text/javascript" src="javascripts/jquery.js"></script>
 <script type="text/javascript" src="javascripts/locastyle.js"></script>
+<script type="text/javascript" src="javascripts/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
