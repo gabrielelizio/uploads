825cc0ea9a9eac8042c2df547b10c8325616d3f6
Lucas Martins <lucas03188@hotmail.com>
2019-09-01 14:16:05 -0300

    alteração da pagina principal de uploads

--- upload/index.php
+++ upload/index.php
@@ -16,69 +16,85 @@
 			<link href="../stylesheets/locastyle.css" rel="stylesheet" type="text/css">
 			<link rel="icon" sizes="192x192" href="../images/ico-boilerplate.png">
 			<link rel="apple-touch-icon" href="../images/ico-boilerplate.png">
-
+			<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  			<link rel="stylesheet" href="../css/all.min.css">
+			<style>
+				
+			</style>
 		</head>
 		<body>
-			<div class="ls-topbar ">
+		
+		<nav style="background-color: #f15c2f;" class="navbar navbar-expand-lg fixed-top navbar-dark conatiner">
+        <a href="#" class="navbar-brand"> <img src="../images/header_logo.png"> Upload de Arquivos </a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#menu" >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            <!-- Linkss -->
+            <div id="menu" class="collapse navbar-collapse container">
+                <ul class="navbar-nav ml-md-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#"> <h6> #Dashboard </h6> </a>
+					</li>
+					<li class="nav-item">
+                        <a class="nav-link"  href="#"> <h6> Processando </h6> </a>
+					</li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> <h6> Realizados </h6> </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"  href="#"> <h6> Erros </h6> </a>
+					</li>
+					
+					<li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="menu_dropdown" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false" style="font-size: 12pt;">  Sair </a>
 
-	<?php  include_once("../notification_bars.php"); ?>
-
-		<span class="ls-show-sidebar ls-ico-menu"></span>
-
-		<!-- Nome do produto/marca com sidebar -->
-			<h1 class="ls-brand-name">
-				<a href="#" class="ls-ico-earth">
-					<small><?php echo "$vg_title";?></small>
-					Provas
-				</a>
-			</h1>
-
-		<!-- Nome do produto/marca sem sidebar quando for o pre-painel  -->
+                        <div style="background-color: black;" class="dropdown-menu text-center">
+                            <a href="../logout.php" class="dropdown-link"> <span class="text-left text-light"> Encerrar Sessão </span> </a> <br>
+                        </div>
+					</li>
+					<li class="nav-item">
+                        <a class="nav-link"  href="#"> <h6></h6> </a>
+					</li>
+					
+                </ul>
+			</div>
+	</nav>
+	<div class="pt-2"></div>
+	<div class=" container-fluid pt-5">
+	<h1 class="ls-title-intro ls-ico-upload pl-5 text-right "> Upload de Provas </h1>
 	</div>
+	<div class="container pt-5">
+					
 
-	<aside class="ls-sidebar">
+		<div class="row">
+			<div class="col-1"></div>
+			<div class="col-10">
 
-		<div class="ls-sidebar-inner">
-				<a href="#"  class="ls-go-prev"><span class="ls-text">Voltar à lista de serviços</span></a>
+			<form action="save.php" method="post" enctype="multipart/form-data" class="ls-form ls-form-horizontal row">
 
-	<?php
-		if($_SESSION["nivel"] == 1){
-				require_once("../menu-professor.php");
-		}else{
-				require_once("../menu-sicp.php");
-		}
-	?>
-
-		</div>
-	</aside>
-
-
-			<main class="ls-main ">
-				<div class="container-fluid">
-					<h1 class="ls-title-intro ls-ico-upload">Upload de Provas</h1>
-
-
-	<form action="save.php" method="post" enctype="multipart/form-data" class="ls-form ls-form-horizontal row">
-		<fieldset>
-			<label class="ls-label col-md-3">
+			<label class="ls-label col-6 ">
 				<b class="ls-label-text">Nome</b>
 				<p class="ls-label-info"></p>
 				<input style="background: #efe7e7;" type="text" name="nome" readonly value="<?= $nomeUpload ?>" required >
 			</label>
-			<label class="ls-label col-md-4">
+			<label class="ls-label col-6 ">
 				<b class="ls-label-text">E-mail</b>
 				<p class="ls-label-info"></p>
 				<input style="background: #efe7e7;" type="text" name="email" readonly value="<?= $emailUpload ?>" required >
 			</label>
 
 			<input style="background: #efe7e7;" type="hidden" name="telefone" readonly value="<?= $telUpload ?>" required >
-
-		</fieldset>
+			</div>
+			<div class="col-1"></div>
+		</div>
 
 		<hr>
 
-		<b class="ls-label-text">Para cadastrar um novo Curso, informe o nome abaixo:</b>
-<!--
+	<div class="row">
+		<b class="ls-label-text col-12" >Para cadastrar um novo Curso, informe o nome abaixo:</b>
+	</div>	
+<!--<
 	<fieldset>
 		<div class="ls-label col-md-5">
 		<form action="cadastrocurso.php">
@@ -89,208 +105,265 @@
 	</fieldset>
 	<fieldset> -->
 			<!-- Exemplo com Checkbox -->
-			<div class="ls-label col-md-5">
-				<p>Selecione o(s) cursos</p>
-				<div>
-				<label class="ls-label-text">
+		<div class="row">
+		<p>Selecione o(s) cursos</p>
+		</div>
+
+		<div class="row">  <!-- Linha 1 -->
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="Administracao">
 					Administração
 				</label>
-				</div>
-				<div>
-				<label class="ls-label-text">
+			</div>
+			
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="Eng. eletrica">
 					Eng. Elétrica
 				</label>
-				</div>
-				<div>
-				<label class="ls-label-text">
+			</div>
+
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="Eng. Mecanica">
 					Eng. Mecânica
 				</label>
-				</div>
-				<div>
-				<label class="ls-label-text">
+			</div>
+				
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="Redes">
 					Redes Comp.
 				</label>
-				</div>
-	<label class="ls-label-text">
+			</div>		
+		</div>
+
+		<div class="row"> <!-- Linha 2 -->
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="Arquitetura">
 					Arquitetura e Urbanismo
 				</label>
-	<label class="ls-label-text">
+			</div>
+			
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="Contabeis">
 					Ciências Contábeis
 				</label>
-	<label class="ls-label-text">
+			</div>
+
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="EngCivil">
 					Engenharia Civil
 				</label>
-
-	<label class="ls-label-text">
+			</div>
+				
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="EngProducao">
 					Engenharia de Produção
 				</label>
+			</div>		
+		</div>
 
-	<label class="ls-label-text">
+		<div class="row"> <!-- Linha 3 -->
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="Enfermagem">
 					Enfermagem
 				</label>
-
-	<label class="ls-label-text">
+			</div>
+			
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="Fisioterapia">
 					Fisioterapia
 				</label>
+			</div>
 
-	<label class="ls-label-text">
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="EduFisica">
 					Educação Física
-	</label>
-	<label class="ls-label-text">
+				</label>
+			</div>
+				
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="GestaoComercial">
 					Gestão Comercial
-	</label>
+				</label>
+			</div>		
+		</div>
 
-	<label class="ls-label-text">
+		<div class="row"> <!-- Linha 4 -->
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="GestaoRH">
 					Gestão de RH
-	</label>
-
-	<label class="ls-label-text">
+				</label>
+			</div>
+			
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="GestaoFinanceira">
 					Gestão Financeira
-	</label>
+				</label>
+			</div>
 
-	<label class="ls-label-text">
-					<input type="checkbox" name="curso[]" value="EduFisica">
-					Educação Física
-	</label>
-
-	<label class="ls-label-text">
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="Logística">
 					Logística
-	</label>
-
-	<label class="ls-label-text">
+				</label>
+			</div>
+				
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="GestaoMarketing">
 					Gestão em Marketing
-	</label>
+				</label>
+			</div>		
+		</div>
 
-	<label class="ls-label-text">
+		<div class="row"> <!-- Linha 5 -->
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="ConstrucaoEdificios">
 					Construção de Edifícios
-	</label>
-
-	<label class="ls-label-text">
+				</label>
+			</div>
+			
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="SistemasInformacao">
 					Sistemas de Informação
-	</label>
+				</label>
+			</div>
 
-	<label class="ls-label-text">
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="Mecatronica Industrial">
 					Mecatrônica
-	</label>
-
-	<label class="ls-label-text">
+				</label>
+			</div>
+				
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="ProducaoIndustrial">
 					G. da Produção Industrial
-	</label>
+				</label>
+			</div>		
+		</div>
 
-	<label class="ls-label-text">
+		<div class="row"> <!-- Linha 5 -->
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="SegurancaTrabalho">
 					Segurança no Trabalho
-	</label>
-
-	<label class="ls-label-text">
+				</label>
+			</div>
+			
+			<div class="col-3">  
+				<label class="ls-label-text text-primary">
 					<input type="checkbox" name="curso[]" value="PosGraduacao">
 					Pós Graduação
+				</label>
+			</div>
+
+			<div class="col-3">  
+			</div>
+				
+			<div class="col-3">  
+			</div>		
+		</div>
+
+<!-- Fim da tabela de cursos  ################################################################### -->
+<br>
+
+<div class="row pt-5">
+	<label class="ls-label col-4">
+		<b class="ls-label-text">Código das turmas</b>
+		<input class="form-control" type="text" name="codturmas" placeholder="Código da(s) turma(s)" required >
 	</label>
+</div>
 
-			</div>
-		</fieldset>
-
-
-	<hr>
-
-	<fieldset>
-		<label class="ls-label col-md-3">
-				<b class="ls-label-text">Código das turmas</b>
-				<input type="text" name="codturmas" placeholder="Código da(s) turma(s)" required >
+<br>
+<div class="row">
+	<div class="ls-label col-8">
+		<p><b>Escolha uma opção de impressão:</b></p>
+			<label class="ls-label-text">
+				<input type="radio" name="opcaoimpressao" value="FrenteVerso" checked="checked">
+				Frente e Verso
 			</label>
-		</fieldset>
-
-	<hr>
-
-		<fieldset>
-			<!-- Exemplo com Radio button -->
-			<div class="ls-label col-md-5">
-				<p><b>Escolha uma opção de impressão:</b></p>
-				<label class="ls-label-text">
-					<input type="radio" name="opcaoimpressao" value="FrenteVerso" checked="checked">
-					Frente e Verso
-				</label>
-				<label class="ls-label-text">
-					<input type="radio" name="opcaoimpressao" value="Frente">
-					Frente
-				</label>
-
-			</div>
-		</fieldset>
-
-	<hr>
-
-	<fieldset>
-	<label class="ls-label col-md-12 col-sm-12">
-				<b class="ls-label-text">Propósito</b>
-				<div class="ls-custom-select">
-					<select name="proposito" class="ls-select">
-						<option value="Parcial 01">Parcial 01</option>
-						<option value="Oficial 01">Oficial 01</option>
-						<option value="Parcial 02">Parcial 02</option>
-						<option value="Oficial 02">Oficial 02</option>
-						<option value="Segunda Chamada">Segunda Chamada</option>
-						<option value="Exame Espacial">Exame Especial</option>
-						<option value="Outros">Outros</option>
-					</select>
-				</div>
+			<label class="ls-label-text">
+				<input type="radio" name="opcaoimpressao" value="Frente">
+				Frente
 			</label>
-			</fieldset>
+	</div>
+</div>
 
-	<hr>
-
-		<fieldset>
-		<label class="ls-label col-md-3">
-				<b class="ls-label-text">Qtde. cópias de cada arquivo</b>
-				<input type="text" name="qtdecopias" placeholder="Número de cópias" required >
-			</label>
-
-
-		</fieldset>
-
-	<hr>
-
-		<label class="ls-label col-md-6">
-			<div class="ls-prefix-group">
-				<span class="ls-label-text-prefix">Arquivo:</span>
-				<input id="input-b1" name="arquivo[]" type="file" class="file" multiple>
+<br>
+<div class="row">
+	<div class="col-8">
+		<label class="ls-label col-12">
+			<b class="ls-label-text">Propósito</b>
+			<div class="ls-custom-select">
+				<select name="proposito" class="form-control">
+					<option value="Parcial 01">Parcial 01</option>
+					<option value="Oficial 01">Oficial 01</option>
+					<option value="Parcial 02">Parcial 02</option>
+					<option value="Oficial 02">Oficial 02</option>
+					<option value="Segunda Chamada">Segunda Chamada</option>
+					<option value="Exame Espacial">Exame Especial</option>
+					<option value="Outros">Outros</option>
+				</select>
 			</div>
 		</label>
+	</div>
+</div>
 
-	<hr>
-			<div class="ls-actions-btn">
-				<button class="ls-btn">Salvar</button>
-				<button class="ls-btn-danger">Limpar</button>
-			</div>
-		</form>
-			</div>
-				</main>
+<br>
+<div class="row">
+	<label class="ls-label col-6">
+		<b class="ls-label-text">Qtde. cópias de cada arquivo</b>
+		<input class="form-control" type="text" name="qtdecopias" placeholder="Número de cópias" required >
+	</label>
+</div>
 
-			<?php include_once("../notification_message.php"); ?>
+<br>
+<div class="row">
+	<label class="ls-label col-10">
+		<div class="ls-prefix-group">
+			<span class="ls-label-text-prefix">Arquivo:</span>
+			<input  id="input-b1" name="arquivo[]" type="file" class="file" multiple>
+		</div>
+	</label>
+</div>
+<br>
 
+<div class="row pt-5">
+	<div class="col-4"></div>
+	<div class="col-2">
+		<button class="btn btn-success col-12">Salvar</button>
+	</div>
+	<div class="col-2">
+		<button class="ls-btn-danger col-12">Limpar</button>
+	</div>
+	<div class="col-4"></div>
+</div>
+</form>
+
+
+<hr><br>
+<div style="height: 200px;"></div>
 
 			<!-- We recommended use jQuery 1.10 or up -->
 			<script type="text/javascript" src="../javascripts/jquery.js"></script>
 			<script type="text/javascript" src="../javascripts/locastyle.js"></script>
+			<script type="text/javascript" src="../javascripts/bootstrap.bundle.min.js"></script>
 		</body>
 	</html>
