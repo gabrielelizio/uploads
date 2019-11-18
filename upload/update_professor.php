5e71e9d3efaa14a8a8339e22547b93d5c1be1f51
Gabriel Elizio <36629477+gabrielelizio@users.noreply.github.com>
2019-11-18 18:09:10 -0300

    Correcao update Professor
    Corrigido query para update das informacões Professor.

--- upload/update_professor.php
+++ upload/update_professor.php
@@ -4,14 +4,11 @@ include_once ("../lib/verifica-login.php");
 include_once ("../variables_global.php");
 
 
-// $id = intval($_REQUEST['id']);
 $firstname = htmlspecialchars($_REQUEST['firstname']);
 $lastname = htmlspecialchars($_REQUEST['lastname']);
 $phone = htmlspecialchars($_REQUEST['phone']);
 $email = htmlspecialchars($_REQUEST['email']);
 $cpf = htmlspecialchars($_REQUEST['cpf']);
-// $pass = md5(htmlspecialchars($_REQUEST['pass']));
-// $funcao = htmlspecialchars($_REQUEST['funcao']);
 
 
 $sql = "UPDATE users set firstname='$firstname',lastname='$lastname',phone='$phone',email='$email', cpf = '$cpf'
@@ -23,7 +20,7 @@ $query = "SELECT * FROM uploads where id = $idUpload";
 $resultado=conecta($maquina,$usuario,$senha,$banco,$query);
 
 
-if($update="UPDATE uploads SET nome='$nomeUpload',telefone='$phone',email='$email' where id_prof=$idUpload")
+if($update="UPDATE uploads SET nome=(SELECT CONCAT(firstname, ' ',lastname)from users where id= $idUpload),telefone='$phone',email='$email' where id_prof=$idUpload")
 {$resultado=conecta($maquina,$usuario,$senha,$banco,$update);
 	{
 		echo"<script>alert('Dados Alterados com Sucesso!');
@@ -31,11 +28,5 @@ if($update="UPDATE uploads SET nome='$nomeUpload',telefone='$phone',email='$emai
 		</script>";
 		}
 }
-
-
-
-
-
-
 
 ?>
